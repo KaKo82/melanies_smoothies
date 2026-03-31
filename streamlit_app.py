f9b778fa6deec404a0e3615f7eb202884a6767ee
KaKo82 <kohnert.kat@gmail.com>
2026-03-31 14:01:54 +0200

    Update streamlit_app.py
    falschen copy code entfernen

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
 
 #new section for requests
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
